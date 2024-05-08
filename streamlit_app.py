891f39deb2a38056377581bc4c1ac463acceffb0
soma3004 <so.uma2008m@gmail.com>
2024-05-08 08:16:50 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ weight = st.number_input("体重を入力",min_value=1)
 height = st.number_input("身長を入力",min_value=1)
 
 bmi = weight/(height*height)
-st.write("あなたのbmiは"+str(bmi)+"kgです")
+st.write("あなたのbmiは"+str(bmi)+"です")
