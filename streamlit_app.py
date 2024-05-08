4949f0efd53b605e8df1ba48accf235e40739d26
soma3004 <so.uma2008m@gmail.com>
2024-05-08 08:49:48 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("bmi計算アプリ")
 flgcheck_A = st.checkbox('checkbox A')
 flgcheck_B = st.checkbox('checkbox B')
 
-flgcheck_A:
+flgcheck_A
 st.text('checkbox A has checed')
-flgcheck_B:
+flgcheck_B
 st.text('checkbox B has checked')
