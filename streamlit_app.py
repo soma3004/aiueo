ec4472af2c42fe084e44a514c591992ee562c454
soma3004 <so.uma2008m@gmail.com>
2024-05-08 08:14:39 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ st.title("bmi計算アプリ")
 st.write("体重を入力してください")
 
 weight = st.number_input("体重を入力")
+
+height = st.number_input("身長を入力")
+
+bmi = weight / (height*height)
+st.write("あなたのbmiは"＋str(bmi)+"kgです")
