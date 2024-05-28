1d5124b4282c3281d403bbc5920918838a449636
soma3004 <so.uma2008m@gmail.com>
2024-05-28 08:57:21 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
 # Streamlitライブラリをインポート
-import streamlit as st
-
-selected_item = st.selectbox('select item',['A','B','C'])
-
-st.text(selected_item)
-
-selected_item = st.selectbox('select_item',['A'],['B'],['c'], index=2)
-
-st.text('selected_item')
+import streamlit as st 
+st.title('数学問題')
