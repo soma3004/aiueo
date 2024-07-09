6655bd30642769c2e7b216267b38760fc1ac7101
soma3004 <so.uma2008m@gmail.com>
2024-07-09 07:52:08 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 # Streamlitライブラリをインポート
-import streamlit as st 
-st.title('二桁の掛け算')
+# app.py
 
-st.write('二桁の掛け算を解こう')
-if st.button('ガチャを引く!'):
+import streamlit as st
+
+def main():
+    st.title('数学')
+    st.write('公式')
+
+    # 例: ユーザーに名前を入力してもらう
+    username = st.text_input('あなたの名前を入力してください', 'Anonymous')
+
+    st.write(f'こんにちは、{username}さん！')
+
+if __name__ == '__main__':
+    main()
