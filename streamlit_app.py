4e3f44c4a71e74e5a6902a1e16b4f1bc0d68cf70
soma3004 <so.uma2008m@gmail.com>
2024-07-29 14:47:04 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,33 +8,8 @@ def generate_quadratic_equation():
     c = random.randint(1, 10) * random.choice([1,2])
     return a, b, c
 
-def main():
-    st.title('')
-    st.write('以下の二次方程式の解の個数を求めてください：')
+problem = {
+    "question":"a × b",
+    "answer":"ab"
 
-    a, b, c = generate_quadratic_equation()
-
-    st.latex(f"{a}x^2 + {b}x + {c} = 0")
-
-    st.write('解の個数を入力してください：')
-    answer = st.text_input("")
-
-    
-
-    if answer:
-        try:
-            discriminant = b**2 - 4*a*c
-            if discriminant < 0:
-                st.write('実数解は0個です。')
-            elif discriminant == 0: # type: ignore
-                st.write('実数解は1個です。')
-            else:
-                discriminant > 0
-                st.write('実数解は2個です。')
-            
-                
-        except ValueError:
-            st.write('数値を入力してください。')
-
-if __name__ == '__main__':
-    main()
+}
