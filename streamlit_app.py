b2607bb5e3e8252e7cf7842fd24d656ed112af10
soma3004 <so.uma2008m@gmail.com>
2024-07-29 14:57:57 +0000

    あり

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,22 @@ def generate_quadratic_equation():
 problem = {
     "question":"a × b",
     "answer":"ab"
-    
-
 }
+
+def check_answer(user_answer):
+    return user_answer.strip() == problem["answer"]
+
+st.title("掛け算")
+st.write(problem["question"])
+user_input = st.text_input("式の答えを入力してください。")
+
+if st.button("答え合わせ"):
+    if user_input:
+        if check_answer(user_input):
+            st.success("正解です！")
+        else:
+            st.error("不正解です。、もう一度試してみてください。")
+    else:
+        st.warning("回答を入力してください。")
+        
+        
