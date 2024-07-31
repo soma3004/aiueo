945afe3fee69f734761bba22dd60c759e4bcf93c
soma3004 <so.uma2008m@gmail.com>
2024-07-31 14:40:52 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ def main():
         if elapsed_time < game_duration:
             st.write(st.session_state.current_question)
             user_answer = st.text_input("答えを入力:",key="user_answer")
-            
+            st.session_state.user_answer = user_answer
+
             if st.button("答えをチェック"):
                 if user_answer.isdigit() and int(user_answer) == st.session_a * st.session.sesion_state.b:
                     st.session_state.score += 1
@@ -46,6 +47,7 @@ def main():
                 else:
                     st.error("不正解！")
                 st.session_state.a, st.session_state.b, st.session_state.current_question = new_question()
+                st.session_state.user_answer = ""
             st.write(f"現在のスコア：{st.session_state.score}")
             st.write(f"残り時間: {max(0, game_duration - int(elapsed_time))}秒")
 
@@ -53,6 +55,7 @@ def main():
             st.write(f"ゲーム終了！スコア: {st.session_state.scoere}")
             if st.button("再スタート"):
                 st.session_state.game_active = False
+                st.session_state.user_answer = ""
 
 if __name__ == "__main__":
     main()
