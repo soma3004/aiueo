7094250aa91ec2bc50f53f87d8b023530b34e1ac
soma3004 <so.uma2008m@gmail.com>
2024-08-05 08:23:14 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,3 +74,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+st.title("暗算力テスト")
+st.text("１分で何問解けるかテストします")
+options = ["一桁×一桁","二桁×二桁","三桁×三桁"]
+selected_option = st.selectbx("挑戦する内容を選んでください。",options)
+
+if selected_option == "一桁×一桁":
+    st.title("一桁×一桁")
+elif selected_option == "二桁×二桁":
+    st.title("二桁×二桁")
+elif selected_option == "三桁×三桁":
+    st.title("三桁×三桁")
+    
