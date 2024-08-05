23dfdf8a5dc1e37e96219077a562c0e2a46124a3
soma3004 <so.uma2008m@gmail.com>
2024-08-05 08:37:21 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.sidebar.title("難易度選択")
 options = st.sidebar.selectbox('選択してください',['一桁×一桁','二桁×二桁','三桁×三桁'])
-
+button_pressed = st.sidebar.button('決定')
 
 
 st.title("暗算力テスト")
 st.text("１分で何問解けるかテストします")
 
-if st.button("決定"):
+if button_pressed:
     if options == "一桁×一桁":
        st.title("一桁×一桁")
     elif options == "二桁×二桁":
