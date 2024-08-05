5f296115ca6f5d72104516a2689cb1c004088924
soma3004 <so.uma2008m@gmail.com>
2024-08-05 08:34:50 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
+st.sidebar.title("難易度選択")
+options = st.sidebar.selectbox('選択してください',['一桁×一桁','二桁×二桁','三桁×三桁'])
+
+
 
 st.title("暗算力テスト")
 st.text("１分で何問解けるかテストします")
-options = ["一桁×一桁","二桁×二桁","三桁×三桁"]
-selected_option = st.selectbox("挑戦する内容を選んでください。",options)
 
-if selected_option == "一桁×一桁":
-    st.title("一桁×一桁")
-elif selected_option == "二桁×二桁":
-    st.title("二桁×二桁")
-elif selected_option == "三桁×三桁":
-    st.title("三桁×三桁")
+if st.button("決定"):
+    if options == "一桁×一桁":
+       st.title("一桁×一桁")
+    elif options == "二桁×二桁":
+       st.title("二桁×二桁")
+    elif options == "三桁×三桁":
+       st.title("三桁×三桁")
 
