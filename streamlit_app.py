871b11905a1d66f3fdcef3b62944bd482be5e659
soma3004 <so.uma2008m@gmail.com>
2024-08-05 10:00:57 +0000

    あとの

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 
-options =n ["一桁×一桁","二桁×二桁","三桁×三桁"]
+options = ["一桁×一桁","二桁×二桁","三桁×三桁"]
 selected_option = st.selectbox("難易度を選んでください",options)
 if selected_option == "一桁×一桁":
     st.subheader("一桁×一桁")
