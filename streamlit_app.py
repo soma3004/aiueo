e9d9f9655a2f24c5b0afc72b6853bc43004b58c6
soma3004 <so.uma2008m@gmail.com>
2024-08-05 10:14:20 +0000

    あとの

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import random
-
+def generate_random_multiplication_problem1():
+    a = random.randint(1,9)
+    b = random.randint(1,9)
+    question1 = f"{a} × {b}"
+    answer1 = a * b
+    return question1,answer1
 options = ["一桁×一桁","二桁×二桁","三桁×三桁"]
 selected_option = st.selectbox("難易度を選んでください",options)
 if selected_option == "一桁×一桁":
     st.subheader("一桁×一桁")
-    st.button("スタート")
+    if st.button("スタート"):
+        question, answer = generate_random_multiplication_problem1()
+        st.write(f"問題:{question}")
+        user_answer = st.number_input("答えを入力してください")
+        if st.button("答え合わせ"):
+            if user_answer == answer:
+                st.success("正解です！")
+            else:
+                st.error("不正解です。")
+
+    
