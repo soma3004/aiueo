fa6831a7051e6ab2739f2b21a47cb028f77fd2dd
soma3004 <so.uma2008m@gmail.com>
2024-12-01 04:56:37 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ def hannbetusiki():
     b,c,a=random.sample(range(1,100),3)
     b,c,a=sorted([b,c,a], reverse=True)
     A=str(a)+"x^2"
-    st.latex(A)
+    B=str(b)+"x"
+    C=str(c)
+    st.latex(A+B+C)
 hannbetusiki()
     
