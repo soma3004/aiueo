a9f16bb74417b756338e3eccef267325ed385c47
soma3004 <so.uma2008m@gmail.com>
2024-12-01 04:34:40 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 def hannbetusiki():
-    a,b,c=random.suuji(range(1,100),3)
+    a,b,c=random.randint(range(1,100),3)
     a,b,c=sorted([a,c,b], reverse=True)
     print(a,b,c)
     
