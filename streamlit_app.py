1d34876b769dfefd260db228d194b85f7ba3126e
soma3004 <so.uma2008m@gmail.com>
2024-12-01 04:39:41 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import random
 
 def hannbetusiki():
-    a,b,c=random.randint(range(1,100),3)
-    a,b,c=sorted([a,c,b], reverse=True)
+    a,c,b=random.sample(range(1,100),3)
+    a,c,b=sorted([a,c,b], reverse=True)
     print(a,b,c)
     
 hannbetusiki()
